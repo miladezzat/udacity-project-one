5f7f79ddb1e0cec66bb482adb30b28b01eaac4d6
Milad E. Fahmy <miladezzat.f@gmail.com>
2022-02-18 13:55:35 +0200

    feat: 🎸 create cache functionality
    move image to src, and add cache utils for caching images

---
+++ src/utils/cache.ts
@@ -0,0 +1,23 @@
+class Cache {
+    cache: string[];
+
+    constructor() {
+        this.cache = []
+    }
+
+    set(name: string): void {
+        this.cache.push(name);
+    }
+
+    get(name: string): string | undefined {
+        return this.cache.find((element) => element === name);
+    }
+
+    isExist(name: string): boolean {
+        const element = this.get(name);
+        
+        return !!element;
+    }
+}
+
+export default new Cache();
