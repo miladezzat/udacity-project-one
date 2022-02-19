fda23af184c1ca5e4ff875aad5f17f1c6cdbf851
Milad E. Fahmy <miladezzat.f@gmail.com>
2022-02-19 03:19:48 +0200

    feat: 🎸 add dotenv

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,14 @@
+import dotenv from 'dotenv'
 import app from './app';
 
+dotenv.config();
+
+const port = process.env.PORT || 3000;
+
 const bootstrap = () => {
-  app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+  app.listen(port, () => {
+    // eslint-disable-next-line no-console
+    console.log(`Server is running on port: ${port} `);
   });
 };
 
